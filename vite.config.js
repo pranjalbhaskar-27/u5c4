a94e60edefdaee9127d08a45029cfb65341efc3b
Pranjal BHaskar <bhaskarpranjal@gmail.com>
2022-05-02 17:29:38 +0530

    abcd

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()]
 })
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()]
+})
